6c4179e32aafd656d5c1096fdfe1d6cced0733e2
Sam Kim <samkim90274@gmail.com>
2020-02-22 21:51:44 -0500

    Add aws-console-email-autofill.js

---
+++ aws-console-email-autofill.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         AWS Console Email Autofill
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Autofill email for AWS login.
+// @author       Sam Kim
+// @match        https://signin.aws.amazon.com/signin*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.getElementById('resolving_input').value = 'sam+aws@sskymail.com';
+    document.getElementById('next_button').click();
+})();
